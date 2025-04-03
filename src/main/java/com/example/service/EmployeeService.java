c3a0741c2bdb78ba81205b205aaef33a0856dba5
supersarkar <sarkarsd18@gmail.com>
2025-04-04 01:03:37 +0530

    added service file EmployeeService.java

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.example.service;
+
+import com.example.entity.Employee;
+import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Add a new employee
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Get all employees
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // Get employee by ID
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        return optionalEmployee.orElse(null);
+    }
+
+    // Update employee
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        if (employeeRepository.existsById(id)) {
+            updatedEmployee.setId(id);
+            return employeeRepository.save(updatedEmployee);
+        }
+        return null;
+    }
+
+    // Delete employee
+    public boolean deleteEmployee(Long id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
